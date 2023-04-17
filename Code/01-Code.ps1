f9591f043e830f5c17c646600d589eb6da89b460
Sierra <maldonado.sierra.a@gmail.com>
2023-04-17 15:44:46 -0400

    homework

--- Code/01-Code.ps1
+++ Code/01-Code.ps1
@@ -1,5 +1,7 @@
+# Sierra Maldonado worked with Geneva, Justin H, and Nick A
 # Automatic OS Updates
 # https://pureinfotech.com/install-windows-10-update-powershell/
+# https://www.nakivo.com/blog/automate-windows-updates-using-powershell-short-overview/
 
 Install-Module PSWindowsUpdate
 Get-WindowsUpdate -AcceptAll -Install -AutoReboot
