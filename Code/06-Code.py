030c0f6dcddb2800b3c132c332b18c4807d4bed3
Sierra <maldonado.sierra.a@gmail.com>
2023-04-24 15:44:35 -0400

    homework

--- Code/06-Code.py
+++ Code/06-Code.py
@@ -5,16 +5,6 @@
 
 from cryptography.fernet import Fernet
 
-# Prompt User
-Choices = {
-    "Mode 1": "Encrypt F",
-    "Mode 2": "Decrypt F",
-    "Mode 3": "Encrypt M",
-    "Mode 4": "Decrypt M"   
-}
-method_choice = input("Pick one of the following choices: \n Mode 1) Encrypt file\n Mode 2) Decrypt File\n Mode 3) Encrypt Message\n Mode 4) Decrypt Message\n")
-method = Choices.get(method_choice)
-
 # Functions to write key and load key
 def write_key():
     key = Fernet.generate_key()
@@ -26,52 +16,69 @@ def load_key():
 
 # Write key and load key
 write_key()
-key = load_key()
 
-# Function to encrypt a file
-def encrypt_file(file_path, key):
-    with open(file_path, "rb") as file:
-        original_data = file.read()
-    f = Fernet(key)
-    encrypted_data = f.encrypt(original_data)
-    with open(file_path, "wb") as file:
-        file.write(encrypted_data)
+while True:
+    
+    # Prompt User
+    Choices = {
+        "Mode 1": "Encrypt F",
+        "Mode 2": "Decrypt F",
+        "Mode 3": "Encrypt M",
+        "Mode 4": "Decrypt M", 
+        "Mode 5": "Exit" 
+    }
+    method_choice = input("Pick one of the following choices: \n Mode 1) Encrypt file\n Mode 2) Decrypt File\n Mode 3) Encrypt Message\n Mode 4) Decrypt Message\n Mode 5) Exit\n")
+    method = Choices.get(method_choice)
 
-# Function to decrypt a file
-def decrypt_file(file_path, key):
-    with open(file_path, "rb") as file:
-        encrypted_data = file.read()
-    f = Fernet(key)
-    decrypted_data = f.decrypt(encrypted_data)
-    with open(file_path, "wb") as file:
-        file.write(decrypted_data)
+    key = load_key()
 
-# Function to encrypt a message
-def encrypt_message(message, key):
-    f = Fernet(key)
-    encrypted_message = f.encrypt(message.encode())
-    return encrypted_message
+    # Function to encrypt a file
+    def encrypt_file(file_path, key):
+        with open(file_path, "rb") as file:
+            original_data = file.read()
+        f = Fernet(key)
+        encrypted_data = f.encrypt(original_data)
+        with open(file_path, "wb") as file:
+            file.write(encrypted_data)
 
-# Function to decrypt a message
-def decrypt_message(encrypted_message, key):
-    f = Fernet(key)
-    decrypted_message = f.decrypt(encrypted_message)
-    return decrypted_message.decode()
+    # Function to decrypt a file
+    def decrypt_file(file_path, key):
+        with open(file_path, "rb") as file:
+            encrypted_data = file.read()
+        f = Fernet(key)
+        decrypted_data = f.decrypt(encrypted_data)
+        with open(file_path, "wb") as file:
+            file.write(decrypted_data)
 
-if method == "Encrypt F":
-    file_path = input("Please type the file path you'd like to encrypt: ")
-    encrypt_file(file_path, key)
+    # Function to encrypt a message
+    def encrypt_message(message, key):
+        plaintext = message.encode('utf-8')
+        f = Fernet(key)
+        encrypted = f.encrypt(plaintext)
+        print("The encrypted message is: " + encrypted.decode('utf-8'))
 
-if method == "Decrypt F":
-    file_path = input("Please type the file path you'd like to decrypt: ")
-    decrypt_file(file_path, key)
+    # Function to decrypt a message
+    def decrypt_message(encrypted_message, key):
+        ciphertext = encrypted_message.encode('utf-8')
+        f = Fernet(key)
+        decrypted = f.decrypt(ciphertext)
+        print("The decrypted message is: " + decrypted.decode('utf-8'))
 
-if method == "Encrypt M":
-    message = input("Please type the message you'd like to encrypt: ")
-    encrypted_message = encrypt_message(message, key)
-    print(f"Encrypted message: {encrypted_message}")
+    if method == "Encrypt F":
+        file_path = input("Please type the file path you'd like to encrypt: ")
+        encrypt_file(file_path, key)
+        print("Your file has been encrypted")
 
-if method == "Decrypt M":
-    encrypted_message = input("Please type the message you'd like to decrypt: ")
-    decrypted_message = decrypt_message(encrypted_message.encode(), key)
-    print(f"Decrypted message: {decrypted_message}")
+    if method == "Decrypt F":
+        file_path = input("Please type the file path you'd like to decrypt: ")
+        decrypt_file(file_path, key)
+        print("Your file has been decrypted")
+    if method == "Encrypt M":
+        message = input("Please type the message you'd like to encrypt: ")
+        encrypt_message(message, key)
+
+    if method == "Decrypt M":
+        encrypted_message = input("Please type the message you'd like to decrypt: ")
+        decrypt_message(encrypted_message, key)
+    if method == "Exit":
+        break
