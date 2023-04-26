3d2cd3f06ed64feaecf5f5bb1c175e2050c81042
Sierra <maldonado.sierra.a@gmail.com>
2023-04-26 18:22:50 -0400

    homework

--- Code/08-Code.py
+++ Code/08-Code.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # Sierra Maldonado
-# Worked with Geneva, Justin H, and Nick A
+# Worked with Geneva, and Nick A
 # File Encrytption Script
 # Chatgpt -> help with change background / was trying to use local photo
 # https://stackoverflow.com/questions/56974927/permission-denied-trying-to-run-python-on-windows-10
