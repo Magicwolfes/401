85efd448540239315bac9f57ca69079e34117cf7
Sierra <maldonado.sierra.a@gmail.com>
2023-05-08 12:52:28 -0400

    homework

---
+++ Code/16-Code.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# Sierra Maldonado
+# Worked with Geneva, and Nick A
+
+
+# Import
+import time
+
+# Function 1
+
+def mode1():
+    # User inpit word list file path
+    filepath = input("Enter your dictionary filepath:\n")
+    #Open the file 
+    file = open(filepath)
+    line = file.readline()
+    # Iterate through each word in given list
+    while line:
+        line = line.rstrip()
+        word = line
+        time.sleep(1)
+        print(word)
+        line = file.readline()
+    file.close()
+
+def mode2():
+    filepath = input("Enter your dictionary filepath:\n")
+    password = input("Please type your new password: \n")
+    with open(filepath) as file:
+        for line in file:
+            if line.strip() == password:
+                print("Password is weak!")
+                return
+    print("Password is strong!")
+
+while True:
+    answer = input("Mode 1 or Mode 2: ")
+    if answer == "Mode 1":
+        mode1()
+    if answer == "Mode 2":
+        mode2()
+    else:
+        break
+# end
