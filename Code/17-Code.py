469da32768f9f9a90cc1be8acbcdfc448afb0666
Sierra <maldonado.sierra.a@gmail.com>
2023-05-10 11:05:08 -0400

    homework

--- Code/17-Code.py
+++ Code/17-Code.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python3
-# Sierra Maldonado
-# Worked with Geneva, and Nick A
-#ChatGBt -> help with syntax
 
 # Import Libiaries 
 import time
@@ -27,17 +23,17 @@ def connect(password):
     try:
         sshConnection.connect(host, port, user, password,timeout=30, banner_timeout=200)
         print(f"[+] Successfully authenticated with password: {password}")
+        sshConnection.close()
         return True
     except paramiko.AuthenticationException:
         # Wrong password
+        print("Incorrect password, trying new one: ")
         return False
     except Exception as e:
         # Unable to establish SSH connection
         print(f"[-] Exception occurred: {e}")
         return False
-    finally:
-        sshConnection.close()
-
+        
 def mode1():
     # User input word list file path
     filepath = input("Enter your dictionary filepath:\n")
@@ -68,6 +64,7 @@ def mode3():
     filepath = input("Enter your wordlist filepath:\n")
     # if not a real IP address, return
     if not ip():
+        print("Invaild IP address. Please try again. ")
         return
     # open file and use function connect() to plug password 
     with open(filepath, encoding="ISO-8859-1") as file:
