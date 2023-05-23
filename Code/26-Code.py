19d616d38da08a35469e4aba73dfc95116f80ad7
Sierra <maldonado.sierra.a@gmail.com>
2023-05-23 12:04:57 -0400

    homework

--- Code/26-Code.py
+++ Code/26-Code.py
@@ -9,43 +9,44 @@ import os
 import datetime
 import time
 
-target = "8.8.8.8"
+target = "8.8.8.0"
 
-def ping_Status(target):    
-    # Evaluate the response and assign success or failure to the status variable
-    icmp = os.system("ping -c 1 " + target)
-    if icmp == 0:
-        status = "success"
-        print(f"{target} is up!")
-    else:
-        status = "failure"
-        print(f"{target} is down!")
-    # Get the current timestamp and print the status and timestamp
-    currenttime = datetime.datetime.now()
-    print(f"{currenttime} - Status: {status}")
-    return status
+def ping_Status(target):
+    try:
+        # Evaluate the response and assign success or failure to the status variable
+        icmp = os.system("ping -c 1 " + target)
+        if icmp == 0:
+            status = "success"
+            print(f"{target} is up!")
+        else:
+            status = "failure"
+            print(f"{target} is down!")
+        
+        # Get the current timestamp and print the status and timestamp
+        currenttime = datetime.datetime.now()
+        print(f"{currenttime} - Status: {status}")
+        return status
+    except Exception as e:
+        logger.exception("An error occurred")
+        raise e
 
 # Create and configure logger
 logging.basicConfig(filename="Demo.log", format='%(asctime)s %(message)s', filemode='w')
 
-#Create object
+# Create object
 logger = logging.getLogger()
 
 # Setting the threshold
 logger.setLevel(logging.DEBUG)
 
-# test message
-logger.debug("harmless debug")
-logger.info("Information")
-logger.warning("WARNING")
-logger.error("Error")
-logger.critical("oH sHiT, tHeReS a fIrE")
-
-
 while True:
-    # Transmit a single ICMP ping packet to the target
-    ping_Status(target)
-    # Wait for 2 seconds before transmitting another ping packet
-    time.sleep(2)   
-
-
+    try:
+        # Transmit a single ICMP ping packet to the target
+        ping_Status(target)
+        # Wait for 2 seconds before transmitting another ping packet
+        time.sleep(2)
+    except KeyboardInterrupt:
+        # Stop the program if the user interrupts it
+        break
+    except Exception as e:
+        logger.exception("An error occurred")
