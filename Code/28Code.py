371f78f233bcd5666e4b844b70da607bf8ccff67
Sierra Maldonado <122700822+Magicwolfes@users.noreply.github.com>
2023-05-24 12:53:27 -0400

    Update and rename 29Code.py to 28Code.py

--- Code/28Code.py
+++ Code/28Code.py
@@ -10,25 +10,38 @@ import logging.handlers as handlers
 import time
 
 target = "8.8.8.8"
-# Create logging object
-logging.basicConfig(filename="Demo.log", format='%(asctime)s %(message)s', filemode='w')
-# Create object
-loggerV = logging.getLogger("app")
 
-# Setting the threshold
-loggerV.setLevel(logging.INFO)  # Use logging.INFO constant or "INFO" string
+# Create loggers
+logger = logging.getLogger()
 
-# Create log file handlers
-loghandler = handlers.RotatingFileHandler("Demo.log", maxBytes=500, backupCount=2)
-loghandler.setLevel(logging.INFO)  # Use logging.INFO constant or "INFO" string
-loggerV.addHandler(loghandler)
+#print to screen logger
+Slogger = logging.Streamhandler()
 
+#print to file
+Flogger = logging.Filehandler('Demo.log')
+
+#Set levels
+Shandler.setLevel(logging.ERROR)
+Fhandler.setlevel(logging.DEBUG)
+
+# Create formatter 
+SFormat = logger.Format('%(name)s - %(levelname)s - %(message)s')
+FFormat = logger.Format('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add format to handlers 
+Fhandler.setFormat(FFormat)
+Shandler.setFormat(SFormat)
+
+# add handlers to loggers
+Slogger.addHandler(Shandler)
+Flogger.addHandler(Fhandler)
+                   
 def ping_Status(target):
     try:
         # Intentionally raise an exception to simulate an error
         raise ValueError("An intentional error occurred")
         # Evaluate the response and assign success or failure to the status variable
-        icmp = os.system("ping -c 1 " + target)
+        icmp = os.system("ping -c gg1 " + target)
         if icmp == 0
             status = "success"
             print(f"{target} is up!")
@@ -42,7 +55,9 @@ def ping_Status(target):
         return status
 
     except Exception as e:
-      loggerV.exception("An error occurred")
+      Flogger.exception("Debugging now ")
+      Slogger.exception("An error has occurred ")
+    
         raise e
 while True:
     try:
@@ -55,4 +70,7 @@ while True:
         break
 
     except Exception as e:
-        loggerV.exception("An error occurred")
+        Flogger.exception("Debugging now ")
+        Slogger.exception("An error has occurred ")
+
+    
