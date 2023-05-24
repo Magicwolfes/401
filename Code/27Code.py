f31551b45b8867fd2a7213b4a81817e3391188f2
Sierra <maldonado.sierra.a@gmail.com>
2023-05-24 15:54:39 -0400

    spooky

--- Code/27Code.py
+++ Code/27Code.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python3
 # Sierra Maldonado
 # Worked with Geneva, and Nick A
-
-#!/usr/bin/env python3
-# Sierra Maldonado
-# Worked with Geneva, and Nick A
-
 import logging
 import os
 import datetime
