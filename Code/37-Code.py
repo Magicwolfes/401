a6a51ad4c7b3437ac0e3358780815d78fef81898
Sierra <maldonado.sierra.a@gmail.com>
2023-06-07 12:38:24 -0400

    homework

---
+++ Code/37-Code.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# Sierra Maldonado
+# Worked with Geneva, and Nick A
+
+# The below Python script shows one possible method to return the cookie from a site that supports cookies.
+
+import requests
+import webbrowser
+
+# targetsite = input("Enter target site:") # Uncomment this to accept user input target site
+targetsite = "http://www.whatarecookies.com/cookietest.asp" # Comment this out if you're using the line above
+response = requests.get(targetsite)
+cookie = response.cookies
+
+print("Target site is " + targetsite)
+print(cookie)
+
+
+# - Send the cookie back to the site and receive a HTTP response
+new_response = requests.get(targetsite, cookies=cookie)
+
+# - Generate a .html file to capture the contents of the HTTP response
+page = new_response.text
+with open(r'C:\Users\maldo\OneDrive\Documents\Github\Ops-401\Code\cookie.html', 'w') as file:
+    file.write(page)
+    
+# - Open it with Firefox
+webbrowser.get('firefox').open('cookie.html')
